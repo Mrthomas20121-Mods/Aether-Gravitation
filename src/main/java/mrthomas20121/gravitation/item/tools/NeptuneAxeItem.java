d95cedf50df2bfbe073404d0f301fb8d708421ac
Mrthomas20121 <thomas77300@gmail.com>
2023-04-28 16:31:21 +0200

    Update NeptuneAxeItem.java

--- src/main/java/mrthomas20121/gravitation/item/tools/NeptuneAxeItem.java
+++ src/main/java/mrthomas20121/gravitation/item/tools/NeptuneAxeItem.java
@@ -2,11 +2,26 @@ package mrthomas20121.gravitation.item.tools;
 
 import com.aetherteam.aether.item.AetherItems;
 import mrthomas20121.gravitation.util.GraviTiers;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class NeptuneAxeItem extends AxeItem {
     public NeptuneAxeItem() {
         super(GraviTiers.NEPTUNE, 5.0f, -3.0f, new Item.Properties().rarity(AetherItems.AETHER_LOOT));
+    }
+
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        super.appendHoverText(stack, level, components, flag);
+        if (flag.isCreative()) {
+            components.add(AetherItems.SILVER_DUNGEON_TOOLTIP);
+        }
+
     }
 }
