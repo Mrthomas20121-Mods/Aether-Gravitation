e31bf07e956f686799225a253c8530f8e1fe0c9b
Thomas B <thomas77300@gmail.com>
2024-01-21 13:55:03 +0100

    1.1.0 aether fix

--- src/main/java/mrthomas20121/gravitation/item/tools/bronzite/BronziteGlovesItem.java
+++ src/main/java/mrthomas20121/gravitation/item/tools/bronzite/BronziteGlovesItem.java
@@ -3,6 +3,7 @@ package mrthomas20121.gravitation.item.tools.bronzite;
 import com.aetherteam.aether.item.accessories.gloves.GlovesItem;
 import mrthomas20121.gravitation.Gravitation;
 import mrthomas20121.gravitation.item.tools.bronzite.BronziteTools;
+import mrthomas20121.gravitation.util.GravitationArmorMaterials;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
@@ -13,7 +14,7 @@ import java.util.function.Supplier;
 public class BronziteGlovesItem extends GlovesItem {
 
     public BronziteGlovesItem(double punchDamage, String glovesName, Supplier<? extends SoundEvent> glovesSound, Properties properties) {
-        super(punchDamage, glovesName, glovesSound, properties.stacksTo(1));
+        super(GravitationArmorMaterials.BRONZITE, punchDamage, glovesName, glovesSound, properties.stacksTo(1));
 
         this.setRenderTexture(Gravitation.MOD_ID, glovesName);
     }
